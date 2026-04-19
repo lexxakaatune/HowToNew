eccd1cbe09f4b0094f9a5feb9fd7d041afa696c8
Alex Agboli <alexcasimia@gmail.com>
2026-04-19 02:43:35 +0100

    Update Register.tsx

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { User, Mail, Lock, Eye, EyeOff, Shield } from "lucide-react";
 import { registerUser } from "../../services/api"; // <-- your backend API call
 
