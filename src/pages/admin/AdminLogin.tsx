34d5e8e31eb5997d04fd805392599b1163405f57
Alex Agboli <alexcasimia@gmail.com>
2026-04-27 13:09:28 +0100

    Update AdminLogin.tsx

--- src/pages/admin/AdminLogin.tsx
+++ src/pages/admin/AdminLogin.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Lock, User, Eye, EyeOff, Shield } from 'lucide-react';
-import { adminLogin } from '../../services/api';
+//import { adminLogin } from '../../services/api';
 
 const AdminLogin = () => {
   const navigate = useNavigate();
